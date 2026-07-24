1ddeb92f0ce8153c1ffd6fae03f890db9993d100
makazis <labdiennabagi@gmail.com>
2026-07-24 21:52:20 +0300

    fixed a single bug

--- Non-Combat/progression/encounter.gd
+++ Non-Combat/progression/encounter.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
-@export_enum("Enemy","Elite","Random","Campfire") var type="Enemy"
+@export_enum("Enemy","Elite","Random","Rest","Resource","Graph??") var type="Enemy"
 func setup(_type):
 	type=_type
 	if _type=="Enemy":
-		pass
+		$Img2317.visible=true
+	if _type=="Elite":
+		$Img2318.visible=true
+	if _type=="Random":
+		$Img2319.visible=true
+	if _type=="Rest":
+		$Img2320.visible=true
+	if _type=="Resource":
+		$Img2321.visible=true
+	if _type=="Graph??":
+		$Img2322.visible=true
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- Non-Combat/progression/map.gd
+++ Non-Combat/progression/map.gd
@@ -1,11 +1,100 @@
 extends Node2D
 
 @onready var encounter=preload("res://Non-Combat/progression/encounter.tscn")
+@onready var line=preload("res://Non-Combat/progression/line.tscn")
 # Called when the node enters the scene tree for the first time.
+
+var encounters=[]
 func _ready() -> void:
-	pass # Replace with function body.
+	var last_enemy_count=1
+	for i in range(50):
+		encounters.append([])
+		if i==0:
+			for ii in range(1):
+				var x=2560*((ii+1.)/2)+randi_range(-60,60)
+				var y=400+randi_range(-60,60)
+				encounters[-1].append([create_encounter_at(Vector2(x,y),"Enemy"),[]])
+		else:
+			var enemy_count=randi_range(randi_range(last_enemy_count-1,last_enemy_count),randi_range(last_enemy_count,last_enemy_count+1))
+			enemy_count=max(1,min(5,enemy_count))
+			for ii in range(enemy_count):
+				var x=2560*((ii+(6-enemy_count)/2.)/(6.))+randi_range(-100,100)
+				var y=400+randi_range(-100,100)+i*300
+				encounters[-1].append([create_encounter_at(Vector2(x,y),"Enemy"),[]])
+				var connections=randi_range(1,int(sqrt(last_enemy_count)))
+				connections=1
+				if ii==0 or ii==enemy_count-1:
+					connections=min(connections,2)
+				var over_q=round(float(ii)/enemy_count*last_enemy_count)
+				var min_q=max(0,over_q-1)
+				var max_q=min(last_enemy_count-1,over_q+1)
+				var c=0
+				while connections>0:
+					c+=1
+					var chosen_connection=randi_range(min_q,max_q)
+					if not chosen_connection in encounters[-1][ii][1]:
+						connections-=1
+						encounters[-1][ii][1].append(chosen_connection)
+						create_line_between(encounters[-1][ii][0].global_position,encounters[-2][chosen_connection][0].global_position)
+					if c==100:
+						print(min_q,max_q,int(sqrt(last_enemy_count)),last_enemy_count,enemy_count,ii)
+				
+			for ii in last_enemy_count:
+				var has_connection=false
+				for iii in enemy_count:
+					for iv in range(len(encounters[-1][iii][1])):
+						if encounters[-1][iii][1][iv]==ii:
+							has_connection=true
+				if !has_connection or true:
+					var added_connections=false
+					var sel=randi_range(0,2)
+					for iii in range(3):
+						if !iii==sel:
+							if not(iii-1+ii<=-1 or iii-1+ii>=enemy_count):
+								#print(i," ",iii-1+ii," ",ii)
+								if not ii in encounters[-1][iii-1+ii][1]:
+									added_connections=true
+									#print(encounters[-1][iii-1+ii][1],ii)
+									encounters[-1][iii-1+ii][1].append(ii)
+									create_line_between(encounters[-1][iii-1+ii][0].global_position,encounters[-2][ii][0].global_position,true)
+					if !added_connections:
+						for iii in range(3):
+							if not(iii-1+ii<=-1 or iii-1+ii>=enemy_count):
+								#print(i," ",iii-1+ii," ",ii)
+								if not ii in encounters[-1][iii-1+ii][1]:
+									added_connections=true
+									#print(encounters[-1][iii-1+ii][1],ii)
+									encounters[-1][iii-1+ii][1].append(ii)
+									create_line_between(encounters[-1][iii-1+ii][0].global_position,encounters[-2][ii][0].global_position)
+							else:
+								pass
+								#print(i," ",iii-1+ii," ",ii,"<---")
+			last_enemy_count=enemy_count
 
-
+func create_encounter_at(pos:Vector2,type:String="Enemy"):
+	var temp_enemy=encounter.instantiate()
+	temp_enemy.setup(type)
+	temp_enemy.global_position=pos
+	$map.add_child(temp_enemy)
+	return temp_enemy
+func create_line_between(pos1:Vector2,pos2:Vector2,secondary=false	):
+	var temp_line=line.instantiate()
+	temp_line.set_point_position(0,pos1-Vector2(-2560,-1260)/2)
+	temp_line.set_point_position(1,pos2-Vector2(-2560,-1260)/2)
+	$map.add_child(temp_line)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var mouse_pos=Vector2(0,0)
+var lmp=Vector2(0,0)
+
 func _process(delta: float) -> void:
-	pass
+	mouse_pos=get_viewport().get_mouse_position()
+	var mouse_rel=mouse_pos-lmp
+	if dragging_map:
+		$map.position.y+=mouse_rel.y
+	lmp=mouse_pos
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("lmb"):
+		dragging_map=true
+	if event.is_action_released("lmb"):
+		dragging_map=false
+var dragging_map=false
